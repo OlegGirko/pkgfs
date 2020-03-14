a47fb38599c19da07cad4a944be3a72547145dff
Oleg Girko <ol@infoserver.lv>
2020-03-14 20:40:31 +0000

    Split out print_hex helper into a separate include file.

--- utils/rpminspect.cpp
+++ utils/rpminspect.cpp
@@ -14,43 +14,6 @@
 
 namespace {
 
-    template <typename T> class print_hex_t;
-
-    template<> class print_hex_t<char> {
-        char c_;
-    public:
-        print_hex_t(char c): c_(c) {}
-        friend std::ostream &operator<<(std::ostream &out, print_hex_t ph)
-        {
-            static const char hexchars[] = "0123456789ABCDEF";
-            return out << hexchars[((ph.c_ >> 4) & 0xf)]
-                       << hexchars[ph.c_ & 0xf];
-        }
-    };
-
-    template <typename T> class print_hex_t {
-        const char *begin_;
-        const char *end_;
-    public:
-        print_hex_t(const T &a)
-        : begin_(reinterpret_cast<const char *>(&a))
-        , end_(reinterpret_cast<const char *>(&a) + sizeof(T))
-        {}
-        friend std::ostream &operator<<(std::ostream &out, print_hex_t ph)
-        {
-            for(const char *p = ph.begin_; p != ph.end_; ++p)
-                if (!(out << print_hex_t<char>(*p)))
-                    break;
-            return out;
-        }
-    };
-
-    template <typename T>
-    print_hex_t<T> print_hex(const T &arg)
-    {
-        return print_hex_t<T>(arg);
-    }
-
     template <unsigned int N>
     struct intfield {
         unsigned char bytes[N];
